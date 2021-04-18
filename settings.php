6295869427fc69698d3221d6c6a841b76ee679f5
RuscalWorld <me@ruscalworld.ru>
2021-04-18 17:45:22 +0300

    Обновление настроек

--- settings.php
+++ settings.php
@@ -3,8 +3,8 @@
     define('APP_ID', '');
     define('PRIVATE_KEY_PATH', '');
 
-    define('REPOSITORY_USER', 'Bortexel');
-    define('REPOSITORY_NAME', 'WebsiteContent');
+    define('REPOSITORY_USER', 'bortexel');
+    define('REPOSITORY_NAME', 'website-content');
     define('RULES_BRANCH', 'rules');
 
     define('COMMITTER_NAME', 'RulesBot');
